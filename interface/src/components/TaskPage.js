9f4ccdd24df32e00e56eb41fa23209b432e68de7
willmilne <51725532+willmilne@users.noreply.github.com>
2019-06-13 22:29:26 -0400

    should be ready to add delete on the server side

--- interface/src/components/TaskPage.js
+++ interface/src/components/TaskPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap'
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import Modal from 'react-modal'
 
 import { get, post, put, del } from './../scripts/serverLibrary';
 
@@ -11,7 +12,7 @@ class TaskPage extends React.Component {
     constructor(props) {
         super(props);
         let dueDate;
-        if(this.props.task && this.props.task.due){
+        if (this.props.task && this.props.task.due) {
             dueDate = this.props.task.due;
             dueDate = new Date(dueDate);
         }
@@ -20,25 +21,28 @@ class TaskPage extends React.Component {
             id: this.props.task && this.props.task.id || -1,
             completed: this.props.task.completed || false,
             name: this.props.task.name,
-            description: this.props.task.description
+            description: this.props.task.description,
+            deleteModalIsOpen: false
         };
 
-        if(this.state.id === -1 || this.state.completed !== false){
+        if (this.state.id === -1 || this.state.completed !== false) {
             this.state.completedButtonDisabled = true;
         }
         else {
             this.state.completedButtonDisabled = false;
         }
-        console.log('initial completed state is ' + this.state.completed);
-        console.log('initial id state is ' + this.state.id);
+
         this.handleDateChange = this.handleDateChange.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
         this.saveTask = this.saveTask.bind(this);
         this.completed = this.completed.bind(this);
+        this.openDeleteModal = this.openDeleteModal.bind(this);
+        this.closeDeleteModal = this.closeDeleteModal.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
     }
 
-    saveTask(){
+    saveTask() {
         let task = {
             id: this.state.id,
             name: this.state.name,
@@ -46,7 +50,7 @@ class TaskPage extends React.Component {
             due: moment(this.state.selectedDate).format('M/D/YYYY'),
             completed: this.state.completed
         };
-        if(this.state.id === -1){
+        if (this.state.id === -1) {
             // we are saving a new task, not updating an old one
             post.task(task, {
                 success: (_res) => {
@@ -70,7 +74,7 @@ class TaskPage extends React.Component {
 
     }
 
-    completed(){
+    completed() {
         this.setState({
             completed: true
         }, () => {
@@ -79,13 +83,13 @@ class TaskPage extends React.Component {
         //this.saveTask());
     }
 
-    handleNameChange(event){
+    handleNameChange(event) {
         this.setState({
             name: event.target.value
         });
     }
 
-    handleDescriptionChange(event){
+    handleDescriptionChange(event) {
         this.setState({
             description: event.target.value
         });
@@ -95,6 +99,28 @@ class TaskPage extends React.Component {
         this.setState({
             selectedDate: date
         });
+    }
+
+    openDeleteModal() {
+        this.setState({ deleteModalIsOpen: true });
+    }
+
+    deleteTask(){
+        if (this.state.id === -1) {
+            // we are saving a new task, not updating an old one
+            del.task(this.state.id, {}, {
+                success: (_res) => {
+                    this.props.savedNewTask(_res);
+                },
+                failure: (_res) => {
+
+                }
+            });
+        }
+    }
+
+    closeDeleteModal() {
+        this.setState({ deleteModalIsOpen: false });
     }
 
     render() {
@@ -108,11 +134,11 @@ class TaskPage extends React.Component {
                 <Form>
                     <Form.Group controlId="formTitle">
                         <Form.Label>Task Name</Form.Label>
-                        <Form.Control id="taskTitle" type="text" placeholder={title} onChange={this.handleNameChange}/>
+                        <Form.Control id="taskTitle" type="text" placeholder={title} onChange={this.handleNameChange} />
                     </Form.Group>
                     <Form.Group controlId="formDescription">
                         <Form.Label>Task Description</Form.Label>
-                        <Form.Control id="taskDescription" type="text" placeholder={description} onChange={this.handleDescriptionChange}/>
+                        <Form.Control id="taskDescription" type="text" placeholder={description} onChange={this.handleDescriptionChange} />
                     </Form.Group>
                 </Form>
                 Set Due Date
@@ -123,10 +149,17 @@ class TaskPage extends React.Component {
                 <br></br>
                 {this.state.completed && <b>Great job finishing this task!<br></br></b>}
                 <Button onClick={this.completed} disabled={this.state.completedButtonDisabled}>Task Completed!</Button>
-                <Button>Delete Task</Button>
+                <Button onClick={this.openDeleteModal} disabled={this.state.id === -1}>Delete Task</Button>
                 <br></br>
                 <Button onClick={this.props.backToTasks}>Back to tasks</Button>
                 <Button onClick={this.saveTask}>Save Changes</Button>
+                <Modal isOpen={this.state.deleteModalIsOpen} onRequestClose={this.closeDeleteModal} contentLabel={"Delete this task?"}>
+                    <div>
+                        <h2>Are you sure you want to delete this task?  It cannot be recovered.</h2>
+                        <Button onClick={this.closeDeleteModal}>No</Button>
+                        <Button onClick={this.deleteTask}>Yes</Button>
+                    </div>
+                </Modal>
             </div>
         );
     }
